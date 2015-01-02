d81bd0ad419ab3bdf450bd7e7cf7b64e871aa5c9
D20K <kkd927@nate.com>
2015-01-02 09:31:33 +0000

    Time: 60

--- client.py
+++ client.py
@@ -1,17 +1,22 @@
 import urllib
 import subprocess
+import time
 
-temperature = subprocess.Popen(["sudo", "/home/pi/client/rpiADC", "0"], stdout=subprocess.PIPE)
-humidity = subprocess.Popen(["sudo", "/home/pi/client/rpiADC", "1"], stdout=subprocess.PIPE)
-moisture = subprocess.Popen(["sudo", "/home/pi/client/rpiADC", "2"], stdout=subprocess.PIPE)
-
+serial_number = '2015AAA'
 url = 'http://203.247.166.59/home/update'
 
-params = urllib.urlencode({
-        'serial_number': 'A2FDDF',
-        'temperature': temperature.communicate()[0],
-        'humidity': humidity.communicate()[0],
-	'moisture': moisture.communicate()[0]
-})
+while 1:
+  temperature = subprocess.Popen(["sudo", "/home/pi/client/rpiADC", "0"], stdout=subprocess.PIPE)
+  humidity = subprocess.Popen(["sudo", "/home/pi/client/rpiADC", "1"], stdout=subprocess.PIPE)
+  moisture = subprocess.Popen(["sudo", "/home/pi/client/rpiADC", "2"], stdout=subprocess.PIPE)
 
-data = urllib.urlopen(url, params).read()
+
+  params = urllib.urlencode({
+    'serial_number': serial_number,
+    'temperature': temperature.communicate()[0],
+    'humidity': humidity.communicate()[0],
+    'moisture': moisture.communicate()[0]
+  })
+
+  data = urllib.urlopen(url, params).read()
+  time.sleep(60)
